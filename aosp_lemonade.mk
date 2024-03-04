26b3d9d23193447ad03c17d53c2447c60d72bff7
chandu078 <chandudyavanapelli03@gmail.com>
2024-03-04 19:47:28 +0000

    lemonade: Nameless-AOSP bringup
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- aosp_lemonade.mk
+++ aosp_lemonade.mk
@@ -11,10 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonade device
 $(call inherit-product, device/oneplus/lemonade/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nameless stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+CUSTOM_BUILD_TYPE := Official
+
+PRODUCT_NAME := aosp_lemonade
 PRODUCT_DEVICE := lemonade
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -26,8 +32,5 @@ PRODUCT_SYSTEM_DEVICE := OnePlus9
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 13 TP1A.220905.001 R.159cc58-4e9e-182b9f release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220905.001/R.159cc58-4e9e-182b9f:user/release-keys
